0ca847dceb829ba02c375c8b2d6ad42b5027489b
muhammadrosyidd <muhammadrosyidd>
2025-09-25 16:00:34 +0700

    prak4-2

--- README.md
+++ README.md
@@ -3,4 +3,9 @@
 A new Flutter project.
 
 ![Screenshot hello_world](images/01.png)
+
+Praktikum 4 Langkah 1
 ![Screenshoot Praktikum 4 Langkah 1](images/02.png)
+
+Praktikum 4 Langkah 2
+![Screenshoot Praktikum 4 Langkah 2](images/03.png)
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import './basic_widget/text_widget.dart';
+import './basic_widget/image_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -105,7 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const MyTextWidget(),
+            const MyImageWidget(),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
